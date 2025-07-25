19f83f2a8cb0e529d92bb15e32e5eb0d95633472
jongHyeon0000 <tamxt4047@naver.com>
2025-07-25 11:14:39 +0900

    초기 프로젝트 업로드

---
+++ src/main/java/com/example/mycocktailback/MycocktailBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycocktailback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycocktailBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycocktailBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycocktailback/MycocktailBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycocktailback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycocktailBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
